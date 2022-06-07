fda655ea0bea1aadf92dff3f374d2624e0a78d4c
ReflextionsDev <reflextionsdev@gmail.com>
2022-06-07 15:49:09 -0400

    page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import CustomThemeProvider from './components/CustomThemeProvider';
 import CartPage from './components/pages/CartPage';
 import HomePage from './components/pages/HomePage';
 
 function App() {
+
+  const [page, setPage] = useState('homePage')
+
   return (
     <CustomThemeProvider>
-      {/* <HomePage /> */}
-      <CartPage/>
+      <button onClick={() => setPage('homePage')}>Home Page</button>
+      <button onClick={() => setPage('cartPage')}>Cart Page</button>
+      {page === 'homePage' ? <HomePage/> : <CartPage/>}
     </CustomThemeProvider>
   );
 }
--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,37 +1,100 @@
 import { Box } from '@mui/material';
+import { useState } from 'react';
 import Layout from '../Layout';
 
 
-const ShoppingCart = [
-    {
-        id: '123',
-        title: "Blue Drum Set",
-        description: 'Blue drums description',
-        brand: 'Yamaha',
-        price: 59999,
-        image: 'https://www.yamaha.com/yamahavgn/PIM/Images/19027_12073_1_1200x1200_80813f268e73483818697e99937dbd59.jpg',
-    },
-    {
-        id: '234',
-        title: "Red Drum Set",
-        description: 'Red drums description',
-        brand: 'Yamaha',
-        price: 59999,
-        image: 'https://m.media-amazon.com/images/I/61YlBr7OQfS._AC_SL1500_.jpg',
-    },
+const sampleShoppingCart = [
+
+  {
+    id: '234',
+    title: "Red Drum Set",
+    price: 59999,
+    quantity: 2,
+    image: 'https://m.media-amazon.com/images/I/61YlBr7OQfS._AC_SL1500_.jpg',
+ },
+ {
+  id: '123',
+  title: "Blue Drum Set",
+  price: 59999,
+  quantity: 2,
+  image: 'https://m.media-amazon.com/images/I/61YlBr7OQfS._AC_SL1500_.jpg',
+},
 ];
 
 const CartPage = () => {
 
-    return (
-        <Layout>
-            <Box width={1} display="flex" flexDirection="column" alignItems="center">
-                {ShoppingCart.map(cartItem =>
-                    <Box p={3}>{cartItem.title} - Qty: {cartItem.quantity} - ${cartItem.price / 100}</Box>
-                )}
-            </Box>
-        </Layout>
-    )
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  const addToCart = (product) => {
+
+
+    // does this product already exist in the shopping cart?
+
+    const productFound = shoppingCart.find(cartItem => cartItem.id === product.id)
+    
+    // // If it does, update the quantity of the existing one
+    if(productFound){
+      const newShoppingCart = shoppingCart.map(cartItem => {
+
+        const newQuantity = cartItem.quantity + 1;
+        if(cartItem.id === productFound.id){
+          return {...cartItem, quantity: cartItem.quantity + 1, total: newQuantity * cartItem.price}
+        }
+        return cartItem
+        }
+      );
+
+      return setShoppingCart(newShoppingCart);
+    }
+
+    // // If it does not add it to the end of the list
+  const newShoppingCart = [...shoppingCart, {
+      id: product.id,
+      title:  product.title,
+      price:  product.price,
+      quantity: 1,
+      image:  product.image,
+      total: product.price
+    }];
+
+    setShoppingCart(newShoppingCart)
+  };
+  
+
+  // We want to display whats in the shopping cart.
+  return (
+    <Layout>
+     <Box
+        width={1}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <button onClick={
+          () => addToCart(    {id: '234',
+          title: "Red Drum Set",
+          description: 'Red drums description',
+          brand: 'Yamaha',
+          price: 59999,
+          image: 'https://m.media-amazon.com/images/I/61YlBr7OQfS._AC_SL1500_.jpg',
+        })}>add RED DRUM SET
+        </button>
+        <button onClick={
+          () => addToCart({
+            id: '123',
+            title: "Blue Drum Set",
+            description: 'Blue drums description',
+            brand: 'Yamaha',
+            price: 59999,
+            image: 'https://www.yamaha.com/yamahavgn/PIM/Images/19027_12073_1_1200x1200_80813f268e73483818697e99937dbd59.jpg',
+          })}>add RED BLUE DRUM SET
+        </button>
+        {shoppingCart.map(cartItem =>
+        <Box p={3}>{cartItem.title} - Qty: {cartItem.quantity} - ${cartItem.price / 100} total$: {cartItem.total/100}</Box>
+        )}
+     </Box>
+    </Layout>
+  )
 }
 
-export default CartPage
+export default CartPage;
